7ad4147155cd8ac92eaa65bed04c3d79f43ad5e2
pmoilane <petteri.j.moilanen@student.oulu.fi>
2026-02-13 14:47:05 +0200

    static frontend

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-//app.use(express.static('dist'))
+app.use(express.static('dist'))
 morgan.token('person', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
