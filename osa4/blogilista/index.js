8e871e3d40580eab6f9cf132de175f74d9571787
pmoilane <petteri.j.moilanen@student.oulu.fi>
2026-02-17 19:11:53 +0200

    controllers and models

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -2,36 +2,16 @@ require('dotenv').config()
 let MONGODB_URI = process.env.MONGODB_URI
 const express = require('express')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
