ebf5fcd8733b10a6b700f04ec5c3a820c5590c37
pmoilane <petteri.j.moilanen@student.oulu.fi>
2026-02-17 19:22:02 +0200

    app and config

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,19 +1,6 @@
-require('dotenv').config()
-let MONGODB_URI = process.env.MONGODB_URI
-const express = require('express')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
+const app = require('./app')
+const config = require('./utils/config')
 
-const app = express()
-
-const mongoUrl = MONGODB_URI
-mongoose.connect(mongoUrl, { family: 4 })
-
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
