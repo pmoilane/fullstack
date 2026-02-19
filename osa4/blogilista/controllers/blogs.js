1d496fcc575e9077c2c0b2a2a7b96b1afd594cd9
pmoilane <petteri.j.moilanen@student.oulu.fi>
2026-02-19 18:17:15 +0200

    osa 4b

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,18 +1,50 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
+  if (!body.title || !body.author) {
+    response.status(400).end()
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0
   })
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    response.status(404).end()
+  }
+
+  blog.title = body.title
+  blog.author = body.author
+  blog.url = body.url
+  blog.likes = body.likes
+
+  const updatedBlog = await blog.save()
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -14,13 +14,95 @@ beforeEach(async () => {
 })
 
 describe('tests for GET "/api/blogs"', () => {
-  test('all blogs are returned as JSON', async () => {
+  test('correct number of JSON blogs are returned', async () => {
     const response = await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
-    console.log(response.body)
     assert.strictEqual(response.body.length, helper.blogs.length)
+  })
+  test('indentifier key for blogs is called id', async () => {
+    const response = await api
+      .get('/api/blogs')
+    const checkResults = response.body.map((blog) => blog.id ? true : false)
+    assert.strictEqual(checkResults.every(check => check === true), true)
+  })
+})
+
+describe('tests for POST "/api/blogs"', () => {
+  test('blogs can be added', async () => {
+    const blogsBeforePOST = await api.get('/api/blogs')
+    const newBlog = {
+      "title": "JavaScript tutorial",
+      "author": "John",
+      "url": "http://blogman.com",
+      "likes": 2
+    }
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const blogsAfterPOST = await api.get('/api/blogs')
+    assert.strictEqual(blogsBeforePOST.body.length + 1, blogsAfterPOST.body.length)
+    const checkBlogs = blogsAfterPOST.body.some(blog => 
+      blog.title === newBlog.title &&
+      blog.author === newBlog.author &&
+      blog.url === newBlog.url &&
+      blog.likes === newBlog.likes
+    )
+    assert.strictEqual(checkBlogs, true)
+  })
+  test('if blog does not have likes field, it gets value 0', async () => {
+    const newBlog = {
+      "title": "JavaScript tutorial",
+      "author": "John",
+      "url": "http://blogman.com"
+    }
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    assert.strictEqual(response.body.likes, 0)
+  })
+  test('if blog does not have a title or url, return status 400 Bad Request', async () => {
+    const newBlog = {
+      "author": "John",
+      "url": "http://blogman.com"
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+    const newBlog2 = {
+      "title": "JavaScript tutorial",
+      "url": "http://blogman.com"
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+})
+describe('tests for DELETE "/api/blogs"', () => {
+  test('delete blog by id', async () => {
+    const blogsBeforeDELETE = await api.get('/api/blogs')
+    await api
+      .delete(`/api/blogs/${blogsBeforeDELETE.body[0].id}`)
+    const blogsAfterDELETE = await api.get('/api/blogs')
+    assert.strictEqual(blogsBeforeDELETE.body.length - 1, blogsAfterDELETE.body.length)
+  })
+})
+describe('tests for PUT "/api/blogs"', () => {
+  test('update blog by id', async () => {
+    const blogs = await api.get('/api/blogs')
+    const updatedBlog = { ...blogs.body[0], likes: 28 }
+    const returnedBlog = await api
+      .put(`/api/blogs/${updatedBlog.id}`)
+      .send(updatedBlog)
+      .expect(200)
+    assert.deepStrictEqual(updatedBlog, returnedBlog.body)
   })
 })
 
